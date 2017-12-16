45db53d9bc0e88eed670903dfa849d968c610e03
antoine1003 <antoine.dautry@free.fr>
2017-12-16 15:18:36 +0100

    Commentaires, amélioration de la méthode getPendingFriendships

--- app/Http/Controllers/MobileController.php
+++ app/Http/Controllers/MobileController.php
@@ -9,49 +9,52 @@ use Illuminate\Support\Facades\Hash;
 
 class MobileController extends Controller
 {
+    /*=======================================
+    =            FONCTIONS USERS            =
+    =======================================*/
+    /**
+     * Retourne tous les utilisateurs
+     * @return User [description]
+     */
     public function getAllUsers()
     {
         app('debugbar')->disable();
-    	$users = User::all();
-    	return json_encode($users);
+        $users = User::all();
+        return json_encode($users);
     }
 
+    /**
+     * Retourne l'utilisateur ayant l'id donné en paramètre.
+     * @param  Request $request [description]
+     * @return User           [description]
+     */
     public function getUserInfoById(Request $request)
     {
         app('debugbar')->disable();
         $id = $request->input('user_id');
         $token = $request->input('token_mobile');
-    	if (config('custom_settings.token_mobile') == $token) {
-    		$user = User::find($id);
-    	}
-    	else
-        {
-    		echo "refused";
-    	}
-    }
-
-    public function getConversationsWithFullname(Request $request)
-    {
-        app('debugbar')->disable();
-        $id = $request->input('id_user');
-        $token = $request->input('token_mobile');
-        if (config('custom_settings.token_mobile') == $token) {            
+        if (config('custom_settings.token_mobile') == $token) {
             $user = User::find($id);
-            if ($user != null) {                
-                $conversations = $user->getConversationsWithFullnameAndUnread();
-                echo json_encode($conversations);
+            if (empty($user)) {
+                return "failed";
             }
             else
             {
-                echo "failed";
+                return '['.json_encode($user).']';
             }
+            
         }
         else
         {
-            echo "refused";
+            return "refused";
         }
     }
 
+    /**
+     * Retourne l'utilisateur ayant l'email donné en paramètre.
+     * @param  Request $request [description]
+     * @return User           [description]
+     */
     public function getUserInfoByEmail(Request $request)
     {
         app('debugbar')->disable();
@@ -62,19 +65,24 @@ class MobileController extends Controller
             $user = User::where('email',$email)->first();
             if(empty($user))
             {
-                echo "failed";
+                return "failed";
             }
             else
             {
-                echo '['.json_encode($user).']';
+                return '['.json_encode($user).']';
             }
         }
         else
         {
-            echo "refused";
+            return "refused";
         }
     }
 
+    /**
+     * Fonction permettant de gérer le changement du mot de passe de l'utilisateur.
+     * @param  Request $request [description]
+     * @return string           [description]
+     */
     public function passwordUpdate(Request $request)
     {
         app('debugbar')->disable();
@@ -83,66 +91,79 @@ class MobileController extends Controller
             if (isset($u)) {
                 $password_old = $request->input('password_old');
                 $password_new = $request->input('password_new');
-                if (Hash::check($request->input('password_old'), $u->password)) {
-                    $u->password = bcrypt($password_new);
-                    $u->save();
-                    echo "success";                
+                if (strlen($password_new) >= 6) {
+                    if (Hash::check($request->input('password_old'), $u->password)) {
+                        $u->password = bcrypt($password_new);
+                        $u->save();
+                        return "success";                
+                    }
+                    else{
+                        //Le old_password ne match pas avec celui dans la BDD
+                        return "failed";
+                    }
                 }
-                else{
-                    echo "failed";
+                else
+                {
+                    //Mdp inférieur à 6 char
+                    return "wrong_size";
                 }
+                
             }
             else
             {
-                echo "failed";
+                //Utilisateur non trouvé
+                return "failed";
             }
          }
         else
         {
-            echo "refused";
+            //Token mismatch
+            return "refused";
         }
     }
+    
+    
+    /*=====  End of FONCTIONS USERS  ======*/
 
-    public function connection(Request $request)
+
+
+    
+   /*==========================================================
+   =            FONCTIONS CONVERSATIONS / MESSAGES            =
+   ==========================================================*/
+   
+   /**
+    * Renvois la liste des conversations concernant l'utilisateur donné en variable POST (id_user).
+    * @param  Request $request [description]
+    * @return Conversation           [description]
+    */
+    public function getConversationsWithFullname(Request $request)
     {
         app('debugbar')->disable();
-    	if (config('custom_settings.token_mobile') == $request->input('token_mobile')) {
-             $u = User::where('email', $request->input('email'))->first();
-
-            if (isset($u)) {            
-                if (Hash::check($request->input('password'), $u->password))
-                {
-                    if($u->verified)
-                    {
-                        if ($u->is_blocked) {
-                            echo "blocked";
-                        }
-                        else
-                        {
-                            echo "success";
-                        }
-                    }
-                    else
-                    {
-                        echo "unverified";
-                    }
-                   
-                }
-                else
-                {
-                    echo "failed";
-                }
+        $id = $request->input('id_user');
+        $token = $request->input('token_mobile');
+        if (config('custom_settings.token_mobile') == $token) {            
+            $user = User::find($id);
+            if ($user != null) {                
+                $conversations = $user->getConversationsWithFullnameAndUnread();
+                return json_encode($conversations);
             }
-            else{
-                echo "failed";
+            else
+            {
+                return "failed";
             }
         }
         else
         {
-            echo "refused";
+            return "refused";
         }
     }
 
+    /**
+     * Ajout d'un message dans une conversation donnée en variable POST.
+     * @param Request $request [description]
+     * @return  string 
+     */
     public function addMessageInConversation(Request $request)
     {
         app('debugbar')->disable();
@@ -161,21 +182,25 @@ class MobileController extends Controller
                 }                
                 $message->contenu = $contenu;
                 $message->save();
-                echo "success";
+                return "success";
             }
             else
             {
-                echo "failed";
+                return "failed";
             }
            
         }
         else
         {
-            echo "refused";
+            return "refused";
         }
     }
     
-
+    /**
+     * Retourne les messages formatés pour un utilisateur et une conversation.
+     * @param  Request $request [description]
+     * @return string|Message           [description]
+     */
     public function getMessagesByUserWithConv(Request $request)
     {
         app('debugbar')->disable();
@@ -186,30 +211,105 @@ class MobileController extends Controller
                 $user = User::find($id_user);
                 $id_conv = $request->input('id_conv');
                 $messages = $user->getMessagesInConv($nb_messages,$id_conv);
-                echo json_encode($messages);
+                return json_encode($messages);
             }
             else
             {
-                echo "failed";
+                return "failed";
             }           
         }
         else
         {
-            echo "refused";
+            return "refused";
         }
     }
+   
+   
+   /*=====  End of FONCTIONS CONVERSATION / MESSAGES  ======*/
+   
+   /*==================================
+   =            CONNECTION            =
+   ==================================*/
+   
+   /**
+    * Gère la connexion de l'utilisateur
+    * @param  Request $request [description]
+    * @return string           [description]
+    */
+    public function connection(Request $request)
+    {
+        app('debugbar')->disable();
+        if (config('custom_settings.token_mobile') == $request->input('token_mobile')) {
+             $u = User::where('email', $request->input('email'))->first();
 
+            if (isset($u)) {            
+                if (Hash::check($request->input('password'), $u->password))
+                {
+                    if($u->verified)
+                    {
+                        if ($u->is_blocked) {
+                            return "blocked";
+                        }
+                        else
+                        {
+                            return "success";
+                        }
+                    }
+                    else
+                    {
+                        return "unverified";
+                    }
+                   
+                }
+                else
+                {
+                    return "failed";
+                }
+            }
+            else{
+                return "failed";
+            }
+        }
+        else
+        {
+            return "refused";
+        }
+    }
+   
+   /*=====  End of CONNECTION  ======*/
+   
+   
 
-
+    /*==================================
+    =            FRIENDSHIP            =
+    ==================================*/
+    
+    /**
+     * Retourne la liste des demandes d'amitier reçues.
+     * Le status vaut forcément 0.
+     * @param  Request $request   [description]
+     * @return Friendship|string  [description]
+     */
     public function getPendingFriendships(Request $request)
     {
         app('debugbar')->disable();
         if (config('custom_settings.token_mobile') == $request->input('token_mobile')) {
-            $user_id = $request->input('id_user');
-            if (isset($user_id)) {                
-                $user = User::find($user_id);
-                $pending =  $user->getFriendRequests();
-                echo json_encode($pending);
+            $id_recipient = $request->input('id_recipient');
+            if (isset($id_recipient)) {                
+                $recipient = User::find($id_recipient);
+                if (isset($recipient)) {
+                    $pending =  $recipient->getFriendRequests();
+                    if (sizeof($pending) > 0) {
+                        return json_encode($pending);
+                    }
+                    else{                        
+                        return "no_pending_friendship";
+                    }
+                }
+                else
+                {
+                    return "user_not_found";
+                }
             }
             else
             {
@@ -218,10 +318,16 @@ class MobileController extends Controller
         }
         else
         {
-            echo "refused";
+            return "refused";
         }
     }
 
+
+    /**
+     * Gestion des demande d'amis. Permet d'accepter, refuser, bloquer ou débloquer une demande.
+     * @param  Request $request [description]
+     * @return string           [description]
+     */
     public function handleFriends(Request $request)
     {
         app('debugbar')->disable();
@@ -231,7 +337,15 @@ class MobileController extends Controller
             if ((isset($id_user) && isset($id_receiver)) && ($id_receiver != $id_sender)) {
                 $user_sender = User::find($id_sender);
                 $user_receiver = User::find($id_receiver);
-                $action = $request->input('id_receiver');
+                /**
+                 * Action a effectué sur la demande :
+                 * - accept
+                 * - deny
+                 * - unblock
+                 * - block
+                 * @var [type]
+                 */
+                $action = $request->input('action');
                 switch ($action) {
                     case 'accept':
                         if ($user_sender->hasFriendRequestFrom($user_receiver)) {
@@ -294,12 +408,21 @@ class MobileController extends Controller
             }
             else
             {
-                echo "failed";
+                return "failed";
             }           
         }
         else
         {
-            echo "refused";
+            return "refused";
         }
     }
+    
+    
+    /*=====  End of FRIENDSHIP  ======*/
+    
+   
+
+
+
+  
 }
